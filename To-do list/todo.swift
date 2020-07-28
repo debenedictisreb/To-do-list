2dbd21fff78add93a7e33f6dfff6f9c4f4a7b447
Rebecca DeBenedictis <rjdebened@verizon.net>
2020-07-28 13:49:12 -0400

    testing...testing 1 2 3

---
+++ To-do list/todo.swift
@@ -0,0 +1,13 @@
+//
+//  todo.swift
+//  To-do list
+//
+//  Created by Rebecca DeBenedictis on 7/28/20.
+//  Copyright © 2020 Rebecca DeBenedictis. All rights reserved.
+//
+
+import UIKit
+class ToDo {
+  var name = ""
+  var important = false
+}
